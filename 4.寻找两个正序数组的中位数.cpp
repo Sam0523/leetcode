830a9ef4137cf72054ea3fa3e5c6bbbdb57889ff
Sam Yu <ytz1995@gmail.com>
2023-05-28 22:41:59 +0800

    4. Median of 2 sorted arrays

--- 4.寻找两个正序数组的中位数.cpp
+++ 4.寻找两个正序数组的中位数.cpp
@@ -9,58 +9,61 @@
 // @lc code=start
 class Solution {
 public:
-    template <typename InputIt>
-    double findMedianSorted(InputIt first, InputIt last) {
-        auto count = std::distance(first, last);
-        if (count != 0)
-            return static_cast<double>(
-                       *std::next(first, count / 2) +
-                       *std::next(first, count / 2 - !(count % 2))) /
-                   2.0;
-        else
-            return numeric_limits<double>::quiet_NaN();
-    }
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        auto first1 = nums1.begin(), last1 = nums1.end();
-        auto first2 = nums2.begin(), last2 = nums2.end();
-        if (nums1.size() == 0)
-            return findMedianSorted(first2, last2);
-        if (nums2.size() == 0)
-            return findMedianSorted(first1, last1);
-
-        size_t count1 = nums1.size(), count2 = nums2.size();
-        for (;;) {
-            double med1 = findMedianSorted(first1, last1);
-            double med2 = findMedianSorted(first2, last2);
-            size_t step = std::min(count1 / 2 - !(count1 % 2),
-                                   count2 / 2 - !(count2 % 2));
-            if (step == 0)
+    int findKthSortedArrays(size_t k,
+                            const vector<int>& nums1,
+                            const vector<int>& nums2) {
+        // find boundary in nums1
+        size_t lo = k > nums2.size() ? k - nums2.size() : 0U;
+        size_t hi = std::min(nums1.size(), k);
+        while (lo < hi) {
+            auto bound1 = (lo + hi) / 2;
+            auto bound2 = k - bound1;
+            if (bound1 < nums1.size() && bound2 > 0 &&
+                nums1[bound1] < nums2[bound2 - 1]) {
+                lo = bound1 + 1;
+            } else if (bound1 > 0 && bound2 < nums2.size() &&
+                       nums1[bound1 - 1] > nums2[bound2]) {
+                hi = bound1 - 1;
+            } else {
+                lo = hi = bound1;
                 break;
-            else if (med1 < med2) {
-                first1 += step;
-                last2 -= step;
-            } else if (med1 > med2) {
-                first2 += step;
-                last1 -= step;
-            } else
-                return med1;
-            count1 = last1 - first1;
-            count2 = last2 - first2;
+            }
         }
+        if (lo >= nums1.size()) {
+            return nums2[k - lo];
+        }
+        if (k - lo >= nums2.size()) {
+            return nums1[lo];
+        }
+        return std::min(nums1[lo], nums2[k - lo]);
+    }
 
-        if (count1 > 4) {
-            first1 += (count1 - 3) / 2;
-            last1 -= (count1 - 3) / 2;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            std::swap(nums1, nums2);
         }
-        if (count2 > 4) {
-            first2 += (count2 - 3) / 2;
-            last2 -= (count2 - 3) / 2;
-        }
-        std::vector<int> v(first1, last1);
-        v.insert(v.end(), first2, last2);
-        std::sort(v.begin(), v.end());
-        return findMedianSorted(v.begin(), v.end());
+        size_t total = nums1.size() + nums2.size();
+        return (findKthSortedArrays(total / 2, nums1, nums2) +
+                findKthSortedArrays((total - 1) / 2, nums1, nums2)) /
+               2.0;
     }
 };
 
 // @lc code=end
+
+int main() {
+    Solution sol;
+
+    assert(sol.findKthSortedArrays(0, {}, {1, 2, 3}) == 1);
+    assert(sol.findKthSortedArrays(1, {}, {1, 2, 3}) == 2);
+    assert(sol.findKthSortedArrays(2, {}, {1, 2, 3}) == 3);
+
+    assert(sol.findKthSortedArrays(0, {1, 1}, {1, 2, 3}) == 1);
+    assert(sol.findKthSortedArrays(1, {1, 1}, {1, 2, 3}) == 1);
+    assert(sol.findKthSortedArrays(2, {1, 1}, {1, 2, 3}) == 1);
+    assert(sol.findKthSortedArrays(3, {1, 1}, {1, 2, 3}) == 2);
+    assert(sol.findKthSortedArrays(4, {1, 1}, {1, 2, 3}) == 3);
+
+    assert(sol.findKthSortedArrays(0, {1, 1}, {1, 1, 1}) == 1);
+    assert(sol.findKthSortedArrays(4, {1, 1}, {1, 1, 1}) == 1);
+}
